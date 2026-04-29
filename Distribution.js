4732fc2a5f4fa70cdb4a8f181d1f620c07932c2d
Putri Dian S <91788765+firareu@users.noreply.github.com>
2026-04-29 16:12:53 +0700

    Solved Client Section

---
+++ Distribution.js
@@ -0,0 +1,23 @@
+document.getElementById("distributionMaps").innerHTML = `
+<div class="general-padding-r-l general-padding-t-b maps-section-top">
+    <div class="general-flex-row">
+        <div class="general-flex-column">
+            <h2 class="general-h2">Jaringan Distribusi</h2>
+            <p class="general-p">Kerjasama bisnis, transaksi perdagangan & distribusi produk ke berbagai wilayah di Indonesia</p>
+        </div>
+        <div class="general-flex-row nominal-layout">
+            <div class="box-red general-flex-column">
+                <p class="general-p nominal-maps-p">14</p>
+                <p class="general-p">Kota</p>
+            </div>
+            <div class="box-red general-flex-column">
+                <p class="general-p nominal-maps-p">5</p>
+                <p class="general-p">Pulau</p>
+            </div>
+        </div>
+        
+    </div>
+
+    <img class=" maps-img" src="assets/image/maps.png" alt="maps" />
+</div>
+`;
---
+++ ProductCategory.js
@@ -0,0 +1,65 @@
+document.getElementById("productCategory").innerHTML = `
+<div class="general-padding-r-l general-padding-t-b product-bottom">
+    <div class="general-flex-column prd-text">
+        <p class="general-p">Apa yang kami tawarkan</p>
+        <h2 class="general-h2">Produk Kami</h2>
+        <a class="btn btn-primary btn-red" href="#" role="button">Lihat Semua Produk</a>
+    </div>
+
+    <div class="general-flex-row">
+        <div class="general-flex-column product-list">
+            <div class="general-flex-row product-list product-space">
+                <div class="img-wrapper">
+                    <h2 class="prd-text-category">Bearing</h2>
+                    <div class="square-red clip-ver-1 general-index"></div>
+                    <img class="clip-ver-1" src="assets/image/overview-1.png" />
+                </div>
+                <div class="img-wrapper">
+                    <h2 class="prd-text-category">Housing<br>Bearing</h2>
+                    <div class="square-blue clip-ver-2 general-index"></div>
+                    <img class='clip-ver-2' src="assets/image/overview-2.png" alt="Overview Perusahaan 2"/>
+                </div>
+            </div>
+            <div class="general-flex-row product-list product-space">
+                <div class="img-wrapper">
+                    <h2 class="prd-text-category">Hollow<br>Besi</h2>
+                    <div class="square-blue clip-ver-2 general-index"></div>
+                    <img class='clip-ver-2' src="assets/image/overview-2.png" alt="Overview Perusahaan 2"/>
+                </div>
+                <div class="img-wrapper">
+                    <h2 class="prd-text-category">Besi<br>Wiremesh</h2>
+                    <div class="square-red clip-ver-1 general-index"></div>
+                    <img class="clip-ver-1" src="assets/image/overview-1.png" />
+                </div>
+            </div>
+        </div>
+
+        <div class="general-flex-column product-list">
+            <div class="general-flex-row product-list product-space">
+                <div class="img-wrapper" href="">
+                    <h2 class="prd-text-category">Besi CNP</h2>
+                    <div class="square-red clip-ver-1 general-index"></div>
+                    <img class="clip-ver-1" src="assets/image/overview-1.png" />
+                </div>
+                <div class="img-wrapper" href="">
+                    <h2 class="prd-text-category">Besi Beton</h2>
+                    <div class="square-blue clip-ver-2 general-index"></div>
+                    <img class='clip-ver-2' src="assets/image/overview-2.png" alt="Overview Perusahaan 2"/>
+                </div>
+            </div>
+            <div class="general-flex-row product-list product-space">
+                <div class="img-wrapper" href="">
+                    <h2 class="prd-text-category">Besi<br>Jilumesh</h2>
+                    <div class="square-blue clip-ver-2 general-index"></div>
+                    <img class='clip-ver-2' src="assets/image/overview-2.png" alt="Overview Perusahaan 2"/>
+                </div>
+                <div class="img-wrapper" href="">
+                    <h2 class="prd-text-category">Besi Plat</h2>
+                    <div class="square-red clip-ver-1 general-index"></div>
+                    <img class="clip-ver-1" src="assets/image/overview-1.png" />
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+`;
