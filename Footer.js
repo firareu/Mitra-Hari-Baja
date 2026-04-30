22dffa400e6c5caec8e6edd1731fa76bedc439f6
Putri Dian S <91788765+firareu@users.noreply.github.com>
2026-04-30 10:32:25 +0700

    Solved Footer and Overview Bg Section

---
+++ Footer.js
@@ -0,0 +1,96 @@
+document.getElementById("footerSection").innerHTML = ` 
+<footer class="bg-dark-blue" id="tempaltemo_footer">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-4 pt-5">
+                <h2 class="h2 text-outfit-bold text-blue border-bottom pb-3 logo">Hari Baja Steel</h2>
+                <ul class="list-unstyled text-light footer-link-list">
+                    <li class="general-flex-row">
+                        <i class="fas fa-map-marker-alt fa-fw align-top"></i>
+                        <p class="text-outfit-regular">Babatan indah No.10 blok A14, Wiyung<br>Surabaya, Jawa Timur, Indonesia</p>
+                    </li>
+                    <li>
+                        <i class="fa fa-phone fa-fw"></i>
+                        <a name="phone" class="text-decoration-none text-light text-outfit-regular" href="tel:088989373100">0813-5792-7589</a>
+                    </li>
+                    <li>
+                        <i class="fa fa-envelope fa-fw"></i>
+                        <a name="email" class="text-decoration-none text-light text-outfit-regular" href="mailto:info@company.com">haribaja.steel@gmail.com</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="col-md-4 pt-5">
+                <h2 class="h2 text-light text-outfit-bold border-bottom pb-3">Produk</h2>
+                <ul class="list-unstyled text-outfit-regular footer-link-list">
+                    <li><a name="god-tears" class="text-decoration-none text-light" href="https://haribajasteel.com/orders/detail/god-tears">Bearing</a></li>
+                    <li><a name="neil-im-strong" class="text-decoration-none text-light" href="https://haribajasteel.com/orders/detail/neil-im-strong">Housing Bearing</a></li>
+                    <li><a name="10th-sense" class="text-decoration-none text-light" href="https://haribajasteel.com/orders/detail/10th-sense">Besi CNP</a></li>
+                    <li><a name="aroma-get-on" class="text-decoration-none text-light" href="https://haribajasteel.com/orders/detail/aroma-get-on">Besi Beton</a></li>
+                    <li><a name="magiscent" class="text-decoration-none text-light" href="https://haribajasteel.com/orders/detail/magiscent">Hollow Besi</a></li>
+                    <li><a name="turboost" class="text-decoration-none text-light" href="https://haribajasteel.com/orders/detail/turboost">Besi Wiremesh</a></li>
+                    <li><a name="turboost" class="text-decoration-none text-light" href="https://haribajasteel.com/orders/detail/turboost">Besi Jilumesh</a></li>
+                    <li><a name="turboost" class="text-decoration-none text-light" href="https://haribajasteel.com/orders/detail/turboost">Besi Plat</a></li>
+                </ul>
+            </div>
+
+            <div class="col-md-4 pt-5">
+                <h2 class="h2 text-light text-outfit-bold border-bottom pb-3">Halaman</h2>
+                <ul class="list-unstyled text-outfit-regular footer-link-list">
+                    <li><a name="home" class="text-decoration-none text-light" href="/">Beranda</a></li>
+                    <li><a name="masterpiece" class="text-decoration-none text-light" href="https://haribajasteel.com/masterpiece">Produk</a></li>
+                    <li><a name="craft" class="text-decoration-none text-light" href="https://haribajasteel.com/craft">Jasa</a></li>
+                    <li><a name="experiment" class="text-decoration-none text-light" href="https://haribajasteel.com/experiment">Pencapaian</a></li>
+                </ul>
+            </div>
+
+        </div>
+
+        <div class="row text-light mb-4">
+            <div class="col-12 mb-3">
+                <div class="w-100 my-3 border-top border-light"></div>
+            </div>
+            <div class="col-auto me-auto">
+                <ul class="list-inline text-left footer-icons">
+                    <li class="list-inline-item text-center">
+                        <a name="footer-facebook" aria-label="facebook" class="nav-icon position-relative text-decoration-none" href="https://www.facebook.com/profile.php?id=61566839376207" target="_blank"><i class="fab fa-facebook-f"></i></a>
+                    </li>
+                    <li class="list-inline-item text-center">
+                        <a name="footer-instagram" aria-label="instagram" class="nav-icon position-relative text-decoration-none" href="https://www.instagram.com/frigglab" target="_blank"><i class="fab fa-instagram"></i></a>
+                    </li>
+                    <li class="list-inline-item text-center">
+                        <a name="footer-twitter" aria-label="twitter" class="nav-icon position-relative text-decoration-none" href="https://x.com/frigglab" target="_blank"><i class="fab fa-twitter"></i></a>
+                    </li>
+                    <li class="list-inline-item text-center">
+                        <a name="footer-tiktok" aria-label="tiktok" class="nav-icon position-relative text-decoration-none" href="https://www.tiktok.com/@frigglab.official" target="_blank"><i class="fab fa-tiktok"></i></a>
+                    </li>
+                    <li class="list-inline-item text-center">
+                        <a name="footer-shopee" aria-label="shopee" class="nav-icon position-relative text-decoration-none" href="https://shopee.co.id/frigglab" target="_blank"><i class="fab fa-shopify"></i></a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-auto">
+                <label class="sr-only text-outfit-regular" for="subscribeEmail">Nomor Whatsapp</label>
+                <div class="input-group mb-2">
+                    <input type="text" class="form-control border-light" id="subscribeEmail" placeholder="Whatsapp Number">
+                    <div class="input-group-text btn-red text-light text-outfit-regular">Konsultasi</div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="w-100 py-3">
+        <div class="container">
+            <div class="row pt-2">
+                <div class="col-12">
+                    <p class="text-left text-light text-outfit-regular">
+                        Copyright © 2026 Hari Baja Steel | Designed by <a name="cr" class="text-decoration-none text-light" rel="sponsored" href="https://autolaris.com" target="_blank">AutoLaris</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</footer>
+`;
